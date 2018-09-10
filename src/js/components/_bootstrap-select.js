b93e46bf69e16c20b022a4c6f8b78021dad1d398
Roman Muratov <myratov.r@gmail.com>
2018-09-10 18:43:51 +0300

    finished compain-details page

--- src/js/components/_bootstrap-select.js
+++ src/js/components/_bootstrap-select.js
@@ -1,3 +1,8 @@
 import selectpicker from 'bootstrap-select';
-
-$('.js-select').selectpicker();
+const select = $('.js-select');
+select.selectpicker()
+  .on('loaded.bs.select', function() {
+    const buttonToggle = $(this).parents('.js-select').find('.dropdown-toggle');
+    const icon = '<svg class="icon icon-arrow-down"><use xlink:href="img/sprite.svg#icon-arrow-down"></use></svg>';
+    buttonToggle.append(icon);
+  });
